b902440887f9930ee184c707b0c570acb518f7fa
MOBSA-Admin <dean.p.taylor+mobsa@gmail.com>
2021-08-04 16:00:32 -0400

    updates to Board page, split on main.js

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -1,50 +1,80 @@
-//Add sponsor carousel
-var sponsors = [];
-$("<div id=\"sponsor_roll_container\"><div class=\"sponsor_roll\"></div></div>").appendTo($("#sponsors"));
-$.ajax({
-    "url": "https://www.leaguelineup.com/websponsors.asp?url=mojbsa",
-    "dataType": "html",
-    "crossDomain": true,
-    "success": function(resp){ 
-        $(resp).find("table a").each(function(i,o){
-            var r = {};
-            var el = $(o).find("img").get(0);
-            r.href = $(o).attr("href");
-            r.name = $(el).attr("alt");
-            r.img = $(el).attr("src");
-            sponsors.push(r);
-            $("<div><a href=\"" + r.href + "\" target=\"_blank\"><img src=\"" + r.img + "\" alt=\"" + r.name + "\" /></a></div>").appendTo($(".sponsor_roll"));
-        });
+$(document).ready(function () {
+	$("#navbar-menu").load("../../assets/templates/newsletter-menu.html");
+	$("#article-footer").load("../../assets/templates/newsletter-article-footer.html"); 
+	$("#footer").load("../../assets/templates/newsletter-footer.html"); 
+	
+	//populate archive
+	$.ajax({
+		url: "../archive.json"
+	}).done(function(data){
+		var num2month = ["","January","February","March","April","May","June","July","August","September","October","November","December"];
+		var root = $("ul.archive");
+		$(data).each(function(i, year){
+			var yearEl = $('<li> \
+								<a class="toggle" href="javascript:void(0);">' + year.year + '</a> \
+								<ul class="inner"></ul> \
+							</li>');
+			root.append(yearEl);
+			var innerYearList = $(yearEl.children().get(1));
+			$(year.months).each(function(i, month){
+				var monthEl = $('<li> \
+									<a class="toggle" href="#">' + num2month[month.month] + '</a> \
+									<ul class="inner"></ul> \
+								</li>');
+				innerYearList.append(monthEl);
+				var innerMonthList = $(monthEl.children().get(1));
+				$(month.articles).each(function(i, article){
+					if(article.active)
+						$('<li><a href="' + article.path + '">' + ( article.title ? article.title : article.date ) + '</a></li>').appendTo(innerMonthList);
+				})
+			})
+		});
 
-        $('.sponsor_roll').slick({
-            autoplay: true,
-            autoplaySpeed: 2000,
-            //arrows: false,
-            initialSlide: Math.floor(Math.random() * sponsors.length),
-            variableWidth: true,
-            centerMode: true,
-            centerPadding: '60px',
-            slidesToShow: 3,
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        arrows: false,
-                        centerMode: true,
-                        centerPadding: '40px',
-                        slidesToShow: 3
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        arrows: false,
-                        centerMode: true,
-                        centerPadding: '40px',
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        });
-    }
+		$('.toggle').click(function(e) {
+			e.preventDefault();
+	
+			var $this = $(this);
+	
+			if ($this.next().hasClass('show')) {
+				$this.next().removeClass('show');
+				$this.next().slideUp(350);
+			} else {
+				$this.parent().parent().find('li .inner').removeClass('show');
+				$this.parent().parent().find('li .inner').slideUp(350);
+				$this.next().toggleClass('show');
+				$this.next().slideToggle(350);
+			}
+		});
+	});
+	
+	var trigger = $('.hamburger'),
+		overlay = $('.overlay'),
+		isClosed = false;
+
+		trigger.click(function (){
+			hamburger_cross();      
+		});
+
+		function hamburger_cross() {
+
+		if (isClosed == true) {          
+			overlay.hide();
+			trigger.removeClass('is-open');
+			trigger.addClass('is-closed');
+			isClosed = false;
+		} else {   
+			overlay.show();
+			trigger.removeClass('is-closed');
+			trigger.addClass('is-open');
+			isClosed = true;
+		}
+	}
+	
+	$('[data-toggle="offcanvas"]').click(function (){
+			$('#wrapper').toggleClass('toggled');
+	});
+
+	$(document).on('click', '.nav .dropdown-manual', function (e) {
+		e.stopPropagation();
+	});
 });
