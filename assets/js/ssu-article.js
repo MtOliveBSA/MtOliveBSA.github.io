ea556bcdc41d2cef801f5f8fa3a7447878bc461e
MOBSA-Admin <dean.p.taylor+mobsa@gmail.com>
2021-09-08 20:55:50 -0400

    updates to SSU common assets

--- assets/js/ssu-article.js
+++ assets/js/ssu-article.js
@@ -2,4 +2,4 @@ $(".subheadline")[0].nextSibling.remove(); /* Update timestamp */
 $(".subheadline")[0].nextSibling.remove(); /* <br/> */
 $(".subheadline")[0].nextSibling.remove(); /* <br/> */
 $('.headline').replaceWith($('#news-header-title'));
-$("img.thumbnail").remove();
+/* $("img.thumbnail").remove(); */
