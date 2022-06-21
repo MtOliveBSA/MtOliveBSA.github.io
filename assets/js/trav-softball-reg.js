bd6c9397a85f48706c9f2588daf35cca7092c33c
MtOliveBSA <admin@mtolivebsa.com>
2022-06-21 09:38:02 -0400

    live test

--- assets/js/trav-softball-reg.js
+++ assets/js/trav-softball-reg.js
@@ -22,15 +22,15 @@ $("#date9 select[name$=_YY]").each(function(i,o){
 	select.change(function(){
 		switch(this.value){
             case "2008":
-            case "2009":    config.division.val("14U"); break;
+            case "2009":    $(config.division).val("14U"); break;
             case "2010":
-            case "2011":    config.division.val("12U"); break;
+            case "2011":    $(config.division).val("12U"); break;
             case "2012":
-            case "2013":    config.division.val("10U"); break;
+            case "2013":    $(config.division).val("10U"); break;
             case "2014":
             case "2015":
-            case "2016":    config.division.val("8U"); break;
-            default:        config.division.val("-");
+            case "2016":    $(config.division).val("8U"); break;
+            default:        $(config.division).val("-");
         }
 	});
 });
