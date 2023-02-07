a053ed9457f325079b8aaaa99298e5fe5e449285
MtOliveBSA <admin@mtolivebsa.com>
2023-02-07 15:10:42 -0500

    sync

--- assets/js/custom-form.js
+++
@@ -1,7 +0,0 @@
-//Update payment method logos
-$("img[src=\"https://www.leaguelineup.com/images/paypal_creditcards.gif\"]").attr("src", "https://mtolivebsa.github.io/images/payment_methods.gif");
-
-//update payment note for detail
-var note = $(".text-danger").filter(function(idx) {
-    return this.innerHTML.indexOf("Credit Card information can be entered once this form is submitted") != -1;
-}).html("<strong>Note</strong>: Payment information can be entered once this form is submitted. If paying via Venmo or Check, please use the Offline Payment option during checkout");
--- assets/js/ssu-article.js
+++
@@ -1,5 +0,0 @@
-$(".subheadline")[0].nextSibling.remove(); /* Update timestamp */
-$(".subheadline")[0].nextSibling.remove(); /* <br/> */
-$(".subheadline")[0].nextSibling.remove(); /* <br/> */
-$('.headline').replaceWith($('#news-header-title'));
-/* $("img.thumbnail").remove(); */
